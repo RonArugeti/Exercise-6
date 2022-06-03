4e460d3ce6109a5478678407477ffd50d96b9fd3
ronius1 <101593555+ronius1@users.noreply.github.com>
2022-06-03 18:55:23 +0300

    Initial Commit

---
+++ Exercise 6/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Exercise 6
+//
+//  Created by Ron Arugeti on 03/06/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
